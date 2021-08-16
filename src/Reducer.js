f781f18894dbe2235abf1622795ca64a9af56ce3
Dhaval Makawana <dhavalmakawana191@gmail.com>
2021-08-16 20:33:35 +0530

    payload

--- src/Reducer.js
+++ src/Reducer.js
@@ -8,7 +8,13 @@ const reducer = (state, action) => {
     return state + 1;
   }
   if (action.type === "DECREMENT") {
-    return state == 0 ? (state = 0) : state - 1;
+    return state === 0 ? (state = 0) : state - 1;
+  }
+  if (action.type === "INCREMENTBY") {
+    return state + action.payload;
+  }
+  if (action.type === "DECREMENTBY") {
+    return state === 0 ? (state = 0) : state - action.payload;
   }
   return state;
 };
@@ -21,8 +27,19 @@ const Reducer = () => {
       <div>
         <h1>{state}</h1>
         <div className="btnStylePosition">
-          <button onClick={() => dispatch({ type: "INCREMENT" })}> Inc </button>
-          <button onClick={() => dispatch({ type: "DECREMENT" })}> Dec </button>
+          <button onClick={() => dispatch({ type: "INCREMENT" })}>
+            Increment
+          </button>
+          <button onClick={() => dispatch({ type: "DECREMENT" })}>
+            Decrement
+          </button>
+          <br></br>
+          <button onClick={() => dispatch({ type: "INCREMENTBY", payload: 5 })}>
+            Increment by 5
+          </button>
+          <button onClick={() => dispatch({ type: "DECREMENTBY", payload: 5 })}>
+            Decrement by 5
+          </button>
         </div>
       </div>
     </>
